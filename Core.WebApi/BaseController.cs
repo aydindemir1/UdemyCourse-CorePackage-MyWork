f8b50ba46790c6bd7fb1c0f8ba33713dbc6848eb
aydin <aydin@AYDIN>
2026-06-08 20:22:23 +0300

    Refactor BaseController and update project references
    - Added `Core.Security.Domain.Entities` and `Core.Security.Extensions` namespaces to `BaseController.cs`.
    - Replaced commented-out methods with active implementations in `BaseController.cs`.
    - Updated `Core.WebApi.csproj`:
      - Incremented version to `3.0.1`.
      - Added project references to `Core.Security.Domain` and `Core.Security.Extensions`.

--- Core.WebApi/BaseController.cs
+++ Core.WebApi/BaseController.cs
@@ -1,7 +1,9 @@
 ﻿using Core.Abstractions.Cqrs;
+using Core.Security.Domain.Entities;
+using Core.Security.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.InteropServices;
 
 
 namespace Core.WebApi
@@ -15,27 +17,27 @@ namespace Core.WebApi
         protected ICqrsProcessor CqrsProcessor => _cqrsProcessor ??= HttpContext.RequestServices.GetService<ICqrsProcessor>() ?? throw new InvalidOperationException("ICqrsProcessor cannot be retrieved from request services");
 
 
-        //protected string getIpAddress()
-        //{
-        //    string ipAddress = Request.Headers.ContainsKey("x-Forwarded-For") ? Request.Headers["x-Forwarded-For"].ToString() : HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString() ?? "Unknown";
-        //    return ipAddress;
-        //}
+        protected string getIpAddress()
+        {
+            string ipAddress = Request.Headers.ContainsKey("x-Forwarded-For") ? Request.Headers["x-Forwarded-For"].ToString() : HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString() ?? "Unknown";
+            return ipAddress;
+        }
 
-        //protected void setRefreshTokenFromCookie(RefreshToken refreshToken)
-        //{
-        //    CookieOptions cookieOptions = new CookieOptions() { HttpOnly = true, Expires = refreshToken.Expires, Secure = true, SameSite = SameSiteMode.Strict };
-        //    Response.Cookies.Append(key: "refreshToken", refreshToken.Token, cookieOptions);
-        //}
+        protected void setRefreshTokenFromCookie(RefreshToken refreshToken)
+        {
+            CookieOptions cookieOptions = new CookieOptions() { HttpOnly = true, Expires = refreshToken.Expires, Secure = true, SameSite = SameSiteMode.Strict };
+            Response.Cookies.Append(key: "refreshToken", refreshToken.Token, cookieOptions);
+        }
 
-        //protected string getRefreshTokenFromCookie()
-        //{
-        //    return Request.Cookies["refreshToken"] ?? throw new ArgumentException("Refresh token is not found");
-        //}
+        protected string getRefreshTokenFromCookie()
+        {
+            return Request.Cookies["refreshToken"] ?? throw new ArgumentException("Refresh token is not found");
+        }
 
-        //protected Guid getUserIdFromRequest()
-        //{
-        //    var userId = HttpContext.User.GetUserId();
-        //    return userId;
-        //}
+        protected Guid getUserIdFromRequest()
+        {
+            var userId = HttpContext.User.GetUserId();
+            return userId;
+        }
     }
 }
